c5931e9586131402fe2b84e739acbf0e90aa8d0a
Alex Wash <Washingtonalex6901@gmail.com>
2024-05-18 19:30:15 -0400

    Fix Dicover Card Stack

--- tori/Views/DiscoverView.swift
+++ tori/Views/DiscoverView.swift
@@ -10,33 +10,61 @@ import SwiftUI
 import CoreLocation
 import SDWebImageSwiftUI
 
+extension View {
+    func cardStack(at position: Int, in total : Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(y: offset * -50)
+    }
+    
+    func cardScale(at position: Int, in total : Int) -> some View {
+        let scale = Double(total - position)
+        return self.scaleEffect(1 - scale * 0.05)
+    }
+    
+    func cardZOffest(at position: Int, in total : Int) -> some View {
+        let zOffset = Double(total - position)
+        return self.zIndex(zOffset * -1)
+    }
+}
+
 struct DiscoverView: View {
     
-    @State private var randomActivity = YelpAPI()
+    @StateObject private var randomActivity = YelpAPI()
     @State private var activitesLoaded: Bool = false
+    @State private var cardOffset: Int = 0
     
     var body: some View {
         GeometryReader{ geoProx in
             ZStack {
-                Color(.clear)
+                Color(.black)
                     .ignoresSafeArea()
                 VStack {
                     
                     if !activitesLoaded {
                         
                         Text("Failed to load activities")
+                            .foregroundStyle(.white)
                             .padding()
                             .frame(maxWidth: geoProx.size.width/1.2, maxHeight: geoProx.size.height/1.2)
-                            .background(.white)
+                            .background(.black)
                             .clipShape(
                                 RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/, style: .continuous)
                             )
-                            .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 10)
+                            .shadow(color: .white.opacity(0.25), radius: 10)
                         
                     } else {
                         ZStack {
-                            ForEach(randomActivity.foundActivities, id: \.self) { card in
-                                CardView(geoProx: geoProx, activityCards: card)
+                            ForEach((randomActivity.foundActivities.indices), id: \.self) { card in
+                                CardView(geoProx: geoProx, activityCards: randomActivity.foundActivities[card], randomActivity: randomActivity) {
+                                    withAnimation {
+                                        removeCard(at: card)
+                                    }
+                                }
+                                .cardStack(at: card, in: randomActivity.foundActivities.count - 1)
+                                .cardScale(at: card, in: randomActivity.foundActivities.count - 1)
+                                .cardZOffest(at: card, in: randomActivity.foundActivities.count - 1)
+                                .allowsHitTesting(card == randomActivity.foundActivities.count - 1)
+                                .ignoresSafeArea()
                             }
                         }
                     }
@@ -45,21 +73,30 @@ struct DiscoverView: View {
             .onAppear {
                 if randomActivity.foundActivities.isEmpty {
                     Task {
-                        await randomActivity.retrieveBusiness(cat: ["axethrowing"], lim: 5, sort: "distance", rad: 40000)
+                        await randomActivity.retrieveBusiness(cat: ["coffee"], lim: 10, sort: "distance", rad: 40000)
                     }
                 }
             }
             .onReceive(randomActivity.$foundActivities) { newActivities in
-                if !newActivities.isEmpty {
+                if (!newActivities.isEmpty) {
                     activitesLoaded = true
+                    for i in randomActivity.foundActivities {
+                        print(i.name ?? "")
+                    }
                     print("New activities retrieved: \(newActivities.count)")
                 } else {
                     activitesLoaded = false
                     print("No activities found.")
                 }
+                
             }
         }
     }
+    
+    func removeCard(at index: Int) {
+            randomActivity.foundActivities.remove(at: index)
+    }
+    
 }
 
 #Preview {
--- tori/Views/SuggestionView.swift
+++ tori/Views/SuggestionView.swift
@@ -10,7 +10,15 @@ import SwiftUI
 
 struct SuggestionView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geoProx in
+            ZStack {
+                Color(.black)
+                    .ignoresSafeArea()
+                VStack {
+                    Text("Tori")
+                }
+            }
+        }
     }
 }
 
